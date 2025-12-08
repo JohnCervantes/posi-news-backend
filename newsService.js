81b42f13036db00d13db9fa6fe9633bd83269f12
John Cervantes <johncervantes@protonmail.com>
2025-12-08 11:44:00 -0600

    small changes to search parameters

--- newsService.js
+++ newsService.js
@@ -1,4 +1,4 @@
-export async function fetchNews({ q = 'positive OR uplifting OR inspiring OR "feel good" OR "good news"', language = 'en', page = 1, pageSize = 20 } = {}) {
+export async function fetchNews({ q = 'positive OR uplifting OR inspiring OR inspiration OR "feel good" OR "good news" OR Heartwarming OR "Acts of kindness" OR Hopeful', language = 'en', page = 1, pageSize = 20 } = {}) {
   if (!process.env.NEWSAPI_KEY) throw new Error('NEWSAPI_KEY not set in environment (set in .env)');
 
   const params = new URLSearchParams({ q, language, page: String(page), pageSize: String(pageSize) });
